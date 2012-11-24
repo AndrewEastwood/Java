3d7c701bff45f8ec0804c07ff4c26d11211c3059
soulcor <admin1@admin1-Z68M-D2H>
2012-11-24 11:49:01 +0200

    mvn pom v1

---
+++ one/src/main/java/com/demo/one/SubApp.java
@@ -0,0 +1,13 @@
+package com.demo.one;
+
+public class SubApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("TROLOLOLO");
+	}
+
+}
