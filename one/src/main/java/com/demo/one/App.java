b9b5b51a107ce9ef51c6a33ee18307051b7e6184
soulcor <admin1@admin1-Z68M-D2H>
2012-11-26 02:22:14 +0200

    maven tutorials, jetty

--- one/src/main/java/com/demo/one/App.java
+++ one/src/main/java/com/demo/one/App.java
@@ -1,15 +1,25 @@
 package com.demo.one;
 
+import org.eclipse.jetty.server.Server;
+
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws Exception
     {
         System.out.println( "Hello World!" );
-        org.apache.log4j.Logger.getRootLogger().info("TROLOLOLOLO");
+        //org.apache.log4j.Logger.getRootLogger().info("TROLOLOLOLO");
+        
+        Server srv = new Server(8089);
+        srv.setHandler(new HelloHandler());
+        srv.start();
+        srv.join();
+        
         
     }
+
 }
--- one/src/test/java/com/demo/one/AppTest.java
+++ one/src/test/java/com/demo/one/AppTest.java
@@ -7,32 +7,44 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest extends TestCase {
+	/**
+	 * Create the test case
+	 * 
+	 * @param testName
+	 *            name of the test case
+	 */
+	public AppTest(String testName) {
+		super(testName);
+	}
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+	/**
+	 * @return the suite of tests being tested
+	 */
+	public static Test suite() {
+		return new TestSuite(AppTest.class);
+	}
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	/**
+	 * Rigourous Test :-)
+	 */
+	public void testApp() {
+		//junit.framework.Assert.fail();
+	}
+
+	public void testApp2() {
+		assertTrue(true);
+	}
+
+	public void testApp3() {
+		assertTrue(true);
+	}
+
+	public void testApp4() {
+		assertTrue(true);
+	}
+
+	public void testApp5() {
+		assertTrue(true);
+	}
 }
