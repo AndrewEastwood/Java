84c8d2b6f763136d26ca3ae44deaacab48b68ac0
AndrewEastwood <soulcor@gmail.com>
2012-11-27 19:46:46 +0200

    local sync

---
+++ mavenTrainings/manager/src/main/java/com/mpws/manager/AppTest.java
@@ -0,0 +1,15 @@
+package com.mpws.manager;
+
+public class AppTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 0; i < 10; i++)
+			System.out
+					.println("TROLOLO TROLOLO TROLOLO TROLOLO TROLOLO TROLOLO TROLOLO ");
+	}
+
+}
---
+++ mavenTrainings/manager/src/main/java/com/mpws/mcommons/managerUtils.java
@@ -0,0 +1,9 @@
+package com.mpws.mcommons;
+
+public class managerUtils {
+
+	public static void showMessage(String msg) {
+		System.out.println(msg);
+	}
+	
+}
